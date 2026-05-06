123c28940c6e894f838934e98d4b128c0a45042e
Moisés Brasão de Almeida <moises.brasao@escolar.ifrn.edu.br>
2026-05-06 19:19:20 +0000

    Aula do dia 06/05 - Introdução à Javascript

---
+++ PPI26.js
@@ -0,0 +1,23 @@
+// # Aula 06/05 - Introdução ao JavaScript
+console.log("Bem vindo a PPI");
+
+// Variáveis
+let name = "João";
+let age = 17;
+let isStudent = true;
+
+const PI = 3.14; // Não pode mudar de valor
+console.log(PI);
+
+// Operadores Aritméticos
+let x = 5 + 5;
+let y = "5" + 5; // soma
+let z = "Hello" + 5; // concatenação
+
+console.log(x, y, z);
+console.log(typeof x);
+
+// Operadores relacionais
+console.log ('5'!=5); // != é a diferença
+console.log ('5'==5); // Compara tipo OU valor
+console.log ('5'===5); // Compara tipo E valor
