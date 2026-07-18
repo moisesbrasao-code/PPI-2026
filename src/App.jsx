9e40fc76426d8fe8e8926837c45bab5a1d8502d1
Moisés Brasão de Almeida <moises.brasao@escolar.ifrn.edu.br>
2026-07-18 20:35:57 +0000

    Aula 15/07 ainda em processo (não click)

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,19 @@ function App() {
     },
   ]);
 
-  function handleToggleFeatured(id) {
+ function handleToggleFeatured(id) {
     setNotices((notices) =>
-      notices.map(
-        (notice) =>
-          notice.id === id ? { ...notice, featured: !notice.featured } : notice,
-      ),
+      notices.map((notice) =>
+        notice.id === id
+          ? { ...notice, featured: !notice.featured }
+          : notice
+      )
+    );
+  }
+
+  function handleDeleteNotice(id) {
+    setNotices((notices) =>
+      notices.filter((notice) => notice.id !== id)
     );
   }
 
@@ -40,7 +47,12 @@ function App() {
         title={"Programação para Internet"}
         subtitle={"Curso Técnico Integrado em Informática"}
       />
-      <NoticeList notices={notices} onToggleFeatured={handleToggleFeatured} />
+
+      <NoticeList
+        notices={notices}
+        onToggleFeatured={handleToggleFeatured}
+        onDeleteNotice={handleDeleteNotice}
+      />
     </>
   );
 }
--- src/components/NoticeCard.jsx
+++ src/components/NoticeCard.jsx
@@ -1,31 +1,44 @@
 import "./NoticeCard.css";
 
-function NoticeCard({ notice, onToggleFeatured }) {
+function NoticeCard({ notice, onToggleFeatured, onDeleteNotice }) {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    // <article className={`notice-card ${notice.featured ? "featured" : ""}`}>
-      <article className={`notice-card ${notice.featured && "featured"}`}>
+    <article className={`notice-card ${notice.featured ? "featured" : ""}`}>
       <p>{notice.category}</p>
       <h3>{notice.title}</h3>
       <p>{notice.description}</p>
+
       <div className="notice-meta">
         <span>Por: {notice.author}</span>
-        -
+        {" - "}
         <span>Data: {notice.date}</span>
       </div>
+
+      {showDetails && (
+        <p>
+          {notice.id === 1
+            ? "A Secitex (Semana de Ciência, Tecnologia e Extensão do IFRN) é um evento que reúne estudantes, servidores e a comunidade para promover a integração através de pesquisas, inovações, apresentações científicas, oficinas e atividades culturais."
+            : "O laboratório de manutenção do Instituto Federal do Rio Grande do Norte (IFRN) - Campus Macau é um espaço essencial para o suporte técnico, combinando atividades práticas de ensino e o reparo de equipamentos da instituição."}
+        </p>
+      )}
+
       <div className="notice-actions">
-        {/* Exibir uma descrição completa do evento. Se ativo, texto do botão = "Fechar Detalhes". */}
-        <button className="details">
-          Exibir Detalhes
-        </button>
         <button onClick={() => onToggleFeatured(notice.id)}>
           {notice.featured ? "Remover destaque" : "Destacar"}
         </button>
-        {/* Excluir o evento da lista */}
-        <button>
+
+        <button
+          className="details"
+          onClick={() => setShowDetails(!showDetails)}
+        >
+          {showDetails ? "Ocultar detalhes" : "Exibir detalhes"}
+        </button>
+
+        <button onClick={() => onDeleteNotice(notice.id)}>
           Excluir
         </button>
       </div>
-
     </article>
   );
 }
